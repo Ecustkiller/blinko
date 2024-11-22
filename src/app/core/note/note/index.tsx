3330d26dcade2b5660b16878ed5a67af01f52521
codexu <461229187@qq.com>
2024-11-22 10:15:26 +0800

    feat: 根据截图生成

--- src/app/core/note/note/index.tsx
+++ src/app/core/note/note/index.tsx
@@ -3,6 +3,7 @@ import { fetchAiStream } from "@/lib/ai"
 import { useEffect, useState } from "react"
 import { MdPreview, Themes } from 'md-editor-rt';
 import { useTheme } from 'next-themes'
+import useMarkStore from "@/stores/mark";
 import { NoteHeader } from './note-header'
 import { NoteFooter } from "./note-footer";
 import 'md-editor-rt/lib/preview.css';
@@ -13,11 +14,14 @@ export function Note() {
   const { theme } = useTheme()
   const [id] = useState('preview-only');
 
+  const { fetchMarks, marks } = useMarkStore()
+
   useEffect(() => {
     setMdTheme(theme as Themes)
   }, [theme])
 
   useEffect(() => {
+    // 根据内容变化滚动到底部
     const md = document.querySelector('#preview-only-preview-wrapper')
     if (md) {
       setTimeout(() => {
@@ -35,8 +39,24 @@ export function Note() {
     }
   }
     
-  function handleAi() {
-    fetchAiStream('写一篇500字的文章，关于截图生成笔记的 app 实现方案。', aiResponse)
+  async function handleAi(customText: string) {
+    await fetchMarks()
+    const request_content = `
+      以下是通过截图后，使用OCR识别出的文字片段：
+      ${marks.map(item => item.content).join(';\n\n')}。
+      请将这些片段整理成一篇详细完整的笔记，要满足以下要求：
+      - 使用 Markdown 语法。
+      - 如果是代码，必须完整保留，不要随意生成。
+      - 笔记片段可能缺失，内容要补全。
+      - 根据笔记内容，延伸出扩展知识。
+      - 笔记顺序可能是错误的，要按照正确顺序排列。
+      - 文字复制的内容尽量不要修改，只处理格式化后的内容。
+      - 你通过图片记录的描述，尽量将图片地址链接到笔记中的匹配位置上，不明确的图片放在文章最后。
+      - 参考资料（带链接，最好是中文网站）
+
+      请满足用户输入的自定义需求：${customText}
+    `
+    fetchAiStream(request_content, aiResponse)
     setText(textChunks)
   }
 
--- src/app/core/note/note/note-footer.tsx
+++ src/app/core/note/note/note-footer.tsx
@@ -5,22 +5,29 @@ import { Textarea } from "@/components/ui/textarea"
 import { Switch } from "@/components/ui/switch"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react"
 
-export function NoteFooter({gen}: {gen: () => void}) {
-  React.useEffect(() => {
+export function NoteFooter({gen}: {gen: (text: string) => void}) {
+  const [text, setText] = useState("")
+
+  useEffect(() => {
     initMarksDb()
   }, [])
 
+  function handleSuccess() {
+    gen(text)
+  }
+
   return (
     <footer className="h-36 border-t p-4">
-      <Textarea placeholder="请输入你的个性化需求（可选）" />
+      <Textarea value={text} onChange={(e) => setText(e.target.value)} placeholder="请输入你的个性化需求（可选）" />
       <div className="flex items-center justify-between mt-4">
         <div className="flex items-center gap-2">
           <Switch id="airplane-mode" />
           <Label htmlFor="airplane-mode">自动生成</Label>
         </div>
         <div className="flex items-center gap-2">
-          <Button onClick={gen}>生成</Button>
+          <Button onClick={handleSuccess}>生成</Button>
         </div>
       </div>
     </footer>
