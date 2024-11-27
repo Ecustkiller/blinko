023ddb1d61e006e367d7c176468d18c4c65a1bf4
codexu <461229187@qq.com>
2024-11-27 17:04:49 +0800

    feat: markdown 编辑器 theme

--- src/app/core/article/md-editor.tsx
+++ src/app/core/article/md-editor.tsx
@@ -1,10 +1,18 @@
 'use client'
-import { MdEditor as MdEditorRT } from 'md-editor-rt';
+import { MdEditor as MdEditorRT, Themes } from 'md-editor-rt';
 import 'md-editor-rt/lib/style.css';
 import useArticleStore from '@/stores/article';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export function MdEditor() {
   const { currentArticle, setCurrentArticle } = useArticleStore()
+  const [mdTheme, setMdTheme] = useState<Themes>('light')
+  const { theme } = useTheme()
 
-  return <MdEditorRT className='flex-1 !h-screen !border-none' noImgZoomIn value={currentArticle} onChange={setCurrentArticle} />;
+  useEffect(() => {
+    setMdTheme(theme as Themes)
+  }, [theme])
+
+  return <MdEditorRT theme={mdTheme} className='flex-1 !h-screen !border-none' noImgZoomIn value={currentArticle} onChange={setCurrentArticle} />;
 }
