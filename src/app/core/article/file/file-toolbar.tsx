696b0579708c551d6e905f6cca15d7e64a0c9fe1
codexu <461229187@qq.com>
2024-12-06 13:28:04 +0800

    feat: 打开文章仓库按钮

--- src/app/core/article/file/file-toolbar.tsx
+++ src/app/core/article/file/file-toolbar.tsx
@@ -1,22 +1,30 @@
 "use client"
 import {TooltipProvider } from "@/components/ui/tooltip"
-import { FilePlus, FolderPlus, FolderSync, PanelRightClose } from "lucide-react"
+import { FilePlus, FolderOpen, FolderPlus, FolderSync } from "lucide-react"
 import * as React from "react"
 import { TooltipButton } from "@/components/tooltip-button"
 import useArticleStore from "@/stores/article"
+import { invoke } from "@tauri-apps/api/core"
+import { appDataDir } from "@tauri-apps/api/path"
 
 export function FileToolbar() {
-  const { newFolder, loadFileTree, newFile } = useArticleStore()
+  const { newFolder, loadFileTree, newFile, activeFilePath } = useArticleStore()
+
+  async function openFolder() {
+    const appDir = await appDataDir()
+    console.log(activeFilePath);
+    invoke('show_in_folder', { path: `${appDir}/article/${activeFilePath}` })
+  }
 
   return (
     <div className="flex justify-between items-center h-12 border-b px-2">
       <div>
-        <TooltipButton icon={<PanelRightClose />} tooltipText="详细视图" />
+        <TooltipButton icon={<FilePlus />} tooltipText="新建文章" onClick={newFile} />
       </div>
       <div className="flex">
         <TooltipProvider>
-          <TooltipButton icon={<FilePlus />} tooltipText="新建文章" onClick={newFile} />
           <TooltipButton icon={<FolderPlus />} tooltipText="新建文件夹" onClick={newFolder} />
+          <TooltipButton icon={<FolderOpen />} tooltipText="仓库" disabled={!activeFilePath} onClick={openFolder} />
           <TooltipButton icon={<FolderSync />} tooltipText="刷新" onClick={loadFileTree} />
         </TooltipProvider>
       </div>
