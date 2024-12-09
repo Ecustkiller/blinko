bed7a42ebf2e32d2fd11a988f6b9e209c956e4a5
codexu <461229187@qq.com>
2024-12-09 09:47:53 +0800

    fix: 未开启同步和配置时，图床页面无法进入

--- src/app/core/image/page.tsx
+++ src/app/core/image/page.tsx
@@ -1,29 +1,50 @@
 'use client'
 import { GithubFile } from '@/lib/github'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { ImageCard } from './image-card'
 import useImageStore from '@/stores/image'
 import useMarkStore from '@/stores/mark'
 import { ImageHeader } from './image-header'
+import useSettingStore from '@/stores/setting'
+import { NoData } from './no-data'
  
 export default function Page() {
+  const [checkSetting, setCheckSetting] = useState(false)
+
   const { images, getImages } = useImageStore()
+  const { githubUsername, accessToken, repositoryName, } = useSettingStore()
   const { fetchAllMarks } = useMarkStore()
+
   useEffect(() => {
-    getImages()
-    fetchAllMarks()
-  }, [])
+    if (checkSetting) {
+      getImages()
+      fetchAllMarks()
+    }
+  }, [checkSetting])
+
+  useEffect(() => {
+    if (githubUsername && accessToken && repositoryName) {
+      setCheckSetting(true)
+    }
+  }, [githubUsername, accessToken, repositoryName])
   
   return (
     <div className="h-screen overflow-auto">
       <ImageHeader />
-      <div className="p-2 grid md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-2">
-        {
-          images.map((file: GithubFile) => (
-            <ImageCard key={file.path} file={file} />
-          ))
-        }
-      </div>
+      {
+        checkSetting ? 
+        (
+          <div className="p-2 grid md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-2">
+            {
+              images.map((file: GithubFile) => (
+                <ImageCard key={file.path} file={file} />
+              )) 
+            }
+          </div>
+        ): 
+        <NoData />
+      }
+      
     </div>
   )
 }
