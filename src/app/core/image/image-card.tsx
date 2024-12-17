e19d65779a4768989be133f087b9f622a0215670
codexu <461229187@qq.com>
2024-12-17 14:20:06 +0800

    feat: 图床管理增加链接复制，支持 Markdown、HTML 复制

--- src/app/core/image/image-card.tsx
+++ src/app/core/image/image-card.tsx
@@ -1,24 +1,22 @@
-import { CircleX, Lock } from "lucide-react"
 import Image from 'next/image'
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardHeader,
 } from "@/components/ui/card"
 import { GithubFile } from "@/lib/github"
-import { Button } from "@/components/ui/button"
 import { convertBytesToSize } from '@/lib/utils'
 import { deleteFile } from "@/lib/github"
 import { toast } from "@/hooks/use-toast"
 import useImageStore from "@/stores/image"
-import useMarkStore from "@/stores/mark"
 import { RepoNames } from "@/lib/github.types"
 import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuSub, ContextMenuSubContent, ContextMenuSubTrigger, ContextMenuTrigger } from "@/components/ui/context-menu"
+import useSettingStore from '@/stores/setting'
 
 export function ImageCard({file}: {file: GithubFile}) {
   const { getImages } = useImageStore()
-  const { allMarks } = useMarkStore()
+  const { githubUsername } = useSettingStore()
+
   async function handleDelete(file: GithubFile) {
     const res = await deleteFile({path: file.path, sha: file.sha, repo: RepoNames.image})
     if (res) {
@@ -28,29 +26,69 @@ export function ImageCard({file}: {file: GithubFile}) {
     }
     getImages()
   }
+
+  async function handleCopyLink() {
+    const fileLink = `https://fastly.jsdelivr.net/gh/${githubUsername}/${RepoNames.image}@main/${file.name}`
+    navigator.clipboard.writeText(fileLink)
+    toast({ title: '已复制 URL 到剪切板', description: fileLink })
+  }
+
+  async function handleCopyMarkdown() {
+    const fileLink = `![${file.name}](https://fastly.jsdelivr.net/gh/${githubUsername}/${RepoNames.image}@main/${file.name})`
+    navigator.clipboard.writeText(fileLink)
+    toast({ title: '已复制 Markdown 到剪切板', description: fileLink })
+  }
+
+  async function handleCopyHTML() {
+    const fileLink = `<img src="https://fastly.jsdelivr.net/gh/${githubUsername}/${RepoNames.image}@main/${file.name}" />`
+    navigator.clipboard.writeText(fileLink)
+    toast({ title: '已复制 HTML 到剪切板', description: fileLink })
+  }
+
   return (
-    <Card className={`w-full h-36 overflow-hidden p-0 rounded-lg shadow-none relative group hover:outline outline-2`}>
-      <CardHeader className="p-0 relative">
-        <PhotoProvider>
-          <PhotoView src={file.download_url}>
-            <Image src={file.download_url} alt={file.name} width={0} height={0} className="w-full h-36 object-cover" />
-          </PhotoView>
-        </PhotoProvider>
-        <div className="absolute top-0 right-0 !mt-0 z-10">
-          {allMarks.map(item => item.url).includes(file.download_url) ?
-            <Button size="icon" disabled><Lock /></Button>: 
-            <Button size="icon" onClick={() => handleDelete(file)}><CircleX /></Button>
-          }
-        </div>
-      </CardHeader>
-      <CardContent className="p-2 gap-1 absolute bottom-0 hidden flex-col bg-secondary-foreground group-hover:flex">
-        <CardDescription className="flex text-xs">
-          名称: {file.name}
-        </CardDescription>
-        <CardDescription className="flex text-xs">
-          大小: {convertBytesToSize(file.size)}
-        </CardDescription>
-      </CardContent>
-    </Card>
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <Card className={`w-full h-36 overflow-hidden p-0 rounded-lg shadow-none relative group hover:outline outline-2`}>
+          <CardContent className="p-0">
+            <PhotoProvider>
+              <PhotoView src={file.download_url}>
+                <Image src={file.download_url} alt={file.name} width={0} height={0} className="w-full h-full object-cover" />
+              </PhotoView>
+            </PhotoProvider>
+          </CardContent>
+        </Card>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuSub>
+          <ContextMenuSubTrigger inset>详细信息</ContextMenuSubTrigger>
+          <ContextMenuSubContent>
+            <ContextMenuItem className='flex justify-between gap-4'>
+              <span className='w-20'>文件名称</span>
+              <span className='text-zinc-500'>{file.name}</span>
+            </ContextMenuItem>
+            <ContextMenuItem className='flex justify-between gap-4'>
+              <span className='w-20'>文件大小</span>
+              <span className='text-zinc-500'>{convertBytesToSize(file.size)}</span>
+            </ContextMenuItem>
+            <ContextMenuItem className='flex justify-between gap-4'>
+              <span className='w-20'>SHA</span>
+              <span className='text-zinc-500'>{file.sha}</span>
+            </ContextMenuItem>
+          </ContextMenuSubContent>
+        </ContextMenuSub>
+        <ContextMenuItem inset onClick={() => handleCopyLink()}>
+          复制链接
+        </ContextMenuItem>
+        <ContextMenuItem inset onClick={() => handleCopyMarkdown()}>
+          复制 Markdown
+        </ContextMenuItem>
+        <ContextMenuItem inset onClick={() => handleCopyHTML()}>
+          复制 HTML
+        </ContextMenuItem>
+        <ContextMenuItem inset onClick={() => handleDelete(file)}>
+          <span className="text-red-900">删除</span>
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   )
 }
