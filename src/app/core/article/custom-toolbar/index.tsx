65f5006d0071fc37ced24d06163ccaf1ca978f8d
codexu <461229187@qq.com>
2024-12-19 09:42:14 +0800

    feat: AI 改为问答模式，只提供问题回复

--- src/app/core/article/custom-toolbar/index.tsx
+++ src/app/core/article/custom-toolbar/index.tsx
@@ -1,6 +1,6 @@
 import { ExposeParam } from "md-editor-rt";
 import { RefObject } from "react";
-import Ai from "./ai";
+import Question from "./question";
 import Optimize from "./optimize";
 import Translation from "./translation";
 import Sync from "./sync";
@@ -9,16 +9,19 @@ import { Separator } from "@/components/ui/separator";
 import Toggle from "./toggle";
 import { Settings } from "./settings.type";
 import Continue from "./continue";
+import Mark from "./mark";
 
 export default function CustomToolbar({mdRef, settings}: {mdRef: RefObject<ExposeParam>, settings: Settings}) {
   return <div className="h-12 w-full border-b flex items-center px-2 gap-1 justify-between">
     <div className="flex h-4 items-center gap-1">
       <Toggle mdRef={mdRef} settings={settings} />
       <Separator orientation="vertical" />
-      <Ai mdRef={mdRef} />
+      <Question mdRef={mdRef} />
       <Continue mdRef={mdRef} />
       <Optimize mdRef={mdRef} />
       <Translation mdRef={mdRef} />
+      <Separator orientation="vertical" />
+      <Mark mdRef={mdRef} />
     </div>
 
     <div>
---
+++ src/app/core/article/custom-toolbar/question.tsx
@@ -0,0 +1,40 @@
+import { TooltipButton } from "@/components/tooltip-button";
+import { toast } from "@/hooks/use-toast";
+import { fetchAiStream } from "@/lib/ai";
+import useArticleStore from "@/stores/article";
+import { MessageCircleQuestion } from "lucide-react";
+import { ExposeParam } from "md-editor-rt";
+import { RefObject } from "react";
+
+export default function Question({mdRef}: {mdRef: RefObject<ExposeParam>}) {
+
+  const { currentArticle, loading, setLoading } = useArticleStore()
+  async function handleBlock() {
+    const selectedText = mdRef.current?.getSelectedText()
+    if (selectedText) {
+      setLoading(true)
+      mdRef.current?.focus()
+      const req = `
+        参考原文：${currentArticle}
+        根据体温：${selectedText}，直接返回回答内容。
+      `
+      let res = ''
+      await fetchAiStream(req, text => {
+        if (text === '[DONE]') return
+        mdRef.current?.insert(() => ({
+          targetValue: res += text,
+        }))
+      })
+      setLoading(false)
+    } else {
+      toast({
+        title: '请先选择一段内容',
+        variant: 'destructive'
+      })
+    }
+  }
+  return (
+    <TooltipButton disabled={loading} icon={<MessageCircleQuestion />} tooltipText="问答" onClick={handleBlock}>
+    </TooltipButton>
+  )
+}
