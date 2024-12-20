47c6c6828e0d047fe8865852230d7ba2a5bcb0bf
codexu <461229187@qq.com>
2024-12-20 11:26:55 +0800

    feat: 写作编辑器上传图片接口

--- src/app/core/article/md-editor.tsx
+++ src/app/core/article/md-editor.tsx
@@ -5,6 +5,8 @@ import { useTheme } from 'next-themes';
 import { useEffect, useRef, useState } from 'react';
 import useSettingStore from '@/stores/setting';
 import CustomToolbar from './custom-toolbar/index';
+import { fileToBase64, uploadFile } from '@/lib/github';
+import { RepoNames } from '@/lib/github.types';
 
 export function MdEditor() {
   const ref = useRef<ExposeParam>(null);
@@ -12,7 +14,7 @@ export function MdEditor() {
   const { currentArticle, setCurrentArticle , saveCurrentArticle, activeFilePath } = useArticleStore()
   const [mdTheme, setMdTheme] = useState<Themes>('light')
   const { theme } = useTheme()
-  const { codeTheme, previewTheme } = useSettingStore()
+  const { codeTheme, previewTheme, githubUsername } = useSettingStore()
   const [toolbar, setToolbar] = useState<ToolbarNames[]>([])
 
   async function handleSave(value: string) {
@@ -22,6 +24,28 @@ export function MdEditor() {
       await saveCurrentArticle(value)
     }
   }
+
+  async function onUploadImg(files: File[], callback: (res: string[]) => void) {
+    const res = await Promise.all(
+      files.map((file) => {
+        return new Promise<string>(async(resolve, reject) => {
+          const fileBase64 = await fileToBase64(file)
+          await uploadFile({
+            ext: file.name.split('.')[1],
+            file: fileBase64,
+            repo: RepoNames.image
+          }).then(async res => {
+            await fetch(`https://purge.jsdelivr.net/gh/${githubUsername}/${RepoNames.image}@main/${res?.data.content.name}`)
+            const url = `https://fastly.jsdelivr.net/gh/${githubUsername}/${RepoNames.image}@main/${res?.data.content.name}`
+            resolve(url)
+          }).catch(err => {
+            reject(err)
+          })
+        });
+      })
+    );
+    callback(res);
+  };
 
   useEffect(() => {
     setMdTheme(theme as Themes)
@@ -52,6 +76,7 @@ export function MdEditor() {
       toolbars={toolbar}
       value={value}
       onChange={handleSave}
+      onUploadImg={onUploadImg}
     />;
   </div>
 }
