a9ab6025ff63e2746ca6014844b4c08793b41326
codexu <461229187@qq.com>
2024-12-20 14:42:13 +0800

    feat: 笔记整理底部增加模型展示，支持跳转设置

--- src/app/core/note/note/note-footer.tsx
+++ src/app/core/note/note/note-footer.tsx
@@ -3,14 +3,14 @@ import * as React from "react"
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
-import { CircleAlert, Loader2 } from "lucide-react"
+import { CircleAlert, Loader2, Package } from "lucide-react"
 import useNoteStore from "@/stores/note"
 import useSettingStore from "@/stores/setting"
 import { useRouter } from "next/navigation";
 export function NoteFooter({gen}: {gen: (text: string) => void}) {
   const [text, setText] = useState("")
   const { loading, setLoading } = useNoteStore()
-  const { apiKey } = useSettingStore()
+  const { apiKey, model } = useSettingStore()
   const router = useRouter()
 
   async function handleSuccess() {
@@ -25,9 +25,14 @@ export function NoteFooter({gen}: {gen: (text: string) => void}) {
   return (
     <footer className="h-36 border-t p-4">
       <Textarea disabled={!apiKey} value={text} onChange={(e) => setText(e.target.value)} placeholder="请输入你的个性化需求（可选）" />
-      <div className="flex items-center justify-between mt-4">
-        <div className="flex items-center gap-2">
-        </div>
+      <div className="flex items-center justify-end mt-4">
+        {
+          (model && apiKey) &&
+          <div className="flex items-center gap-1 mr-4 text-sm text-zinc-500 cursor-pointer hover:underline" onClick={handleSetting}>
+            <Package className="size-4" />
+            {model}
+          </div>
+        }
         <div className="flex items-center gap-2">
           {
             apiKey ? 
