c65faafc90c7f442482278fce4aa9bc010998a66
codexu <461229187@qq.com>
2025-01-06 10:15:28 +0800

    docs: 📝 空聊天记录提示信息

--- src/app/core/record/chat/chat-empty.tsx
+++ src/app/core/record/chat/chat-empty.tsx
@@ -3,19 +3,19 @@ import { Bot, NotebookPen, Clipboard, Link } from 'lucide-react'
 export default function ChatEmpty() {
   const list = [
     {
-      content: <p>与 AI 机器人进行<b>聊天</b>或<b>提问</b></p>,
+      content: <p>与 AI 机器人进行<b>聊天</b></p>,
       icon: <Bot className='size-4' />
     },
     {
-      content: <p><b>关联</b>你的记录和写作内的文章</p>,
+      content: <p>已与你的记录<b>关联</b></p>,
       icon: <Link className='size-4' />
     },
     {
-      content: <p>识别<b>剪贴板</b>，记录插图或文本</p>,
+      content: <p>识别<b>剪贴板</b>记录</p>,
       icon: <Clipboard className='size-4' />
     },
     {
-      content: <p>将你的所有记录<b>整理</b>为笔记</p>,
+      content: <p>将你的记录<b>整理</b>为笔记</p>,
       icon: <NotebookPen className='size-4' />
     },
   ]
