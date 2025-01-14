fbd74e0ec374db5bb228864cf23fc5f2ffe00a45
codexu <461229187@qq.com>
2025-01-14 11:09:16 +0800

    feat: 快捷键设置

--- src/app/core/setting/page.tsx
+++ src/app/core/setting/page.tsx
@@ -4,15 +4,17 @@ import { SettingTab } from "./setting-tab"
 import { SettingAI } from "./setting-ai"
 import { SettingSync } from "./setting-sync"
 import { SettingOCR } from "./setting-ocr"
+import { SettingShortcut } from "./setting-shortcut"
 
 export default function Page() {
   return <div className="flex">
     <SettingTab />
-    <div className="flex-1 p-4">
+    <div className="flex-1 p-4 overflow-y-auto h-screen">
       <SettingAbout />
       <SettingAI />
       <SettingSync />
       <SettingOCR />
+      <SettingShortcut />
     </div>
   </div>
 }
---
+++ src/app/core/setting/setting-shortcut.tsx
@@ -0,0 +1,18 @@
+import { SettingRow, SettingType } from "./setting-base";
+import { Badge } from "@/components/ui/badge";
+
+export function SettingShortcut() {
+
+  return (
+    <SettingType title="快捷键">
+      <SettingRow>
+        <span>
+          截图是快速记录的最佳方式，可以通过全局快捷键快速截图，无需打开主页面。
+        </span>
+        <Badge>
+          CommandOrControl + Shift + S
+        </Badge>
+      </SettingRow>
+    </SettingType>
+  )
+}
