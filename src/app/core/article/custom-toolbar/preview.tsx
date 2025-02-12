68f8d380413bcd671dfd20b5cc239e84974a7183
codexu <461229187@qq.com>
2025-02-12 14:16:41 +0800

    feat: 视图切换按钮优化为 tab 切换

--- src/app/core/article/custom-toolbar/preview.tsx
+++ src/app/core/article/custom-toolbar/preview.tsx
@@ -1,35 +1,29 @@
-import { TooltipButton } from "@/components/tooltip-button";
 import { SquareMIcon, SquareSplitHorizontal, ViewIcon } from "lucide-react";
 import { ExposeParam } from "md-editor-rt";
-import { RefObject, useState } from "react";
+import { RefObject } from "react";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 export default function Preview({mdRef}: {mdRef: RefObject<ExposeParam>}) {
 
   const previewTypes = [
-    {type: 'code', name: '编辑模式', icon: <SquareMIcon />},
-    {type: 'preview', name: '分屏模式', icon: <SquareSplitHorizontal />},
-    {type: 'preview-only', name: '预览模式', icon: <ViewIcon />},
+    {type: 'code', name: '编辑模式', icon: <SquareMIcon className="size-4" />},
+    {type: 'preview', name: '分屏模式', icon: <SquareSplitHorizontal className="size-4" />},
+    {type: 'preview-only', name: '预览模式', icon: <ViewIcon className="size-4" />},
   ]
 
-  const [type, setType] = useState(0)
-
-  async function handler() {
-    let cacheType = type
-    if (type < previewTypes.length - 1) {
-      setType(type + 1)
-      cacheType += 1
-    } else {
-      setType(0)
-      cacheType = 0
-    }
-    switch (cacheType) {
-      case 0:
+  async function handler(tab: string) {
+    switch (tab) {
+      case 'code':
         mdRef.current?.togglePreview(false)
         break;
-      case 1:
+      case 'preview':
         mdRef.current?.togglePreview(true)
         break;
-      case 2:
+      case 'preview-only':
         mdRef.current?.togglePreviewOnly(true)
         break;
       default:
@@ -38,7 +32,16 @@ export default function Preview({mdRef}: {mdRef: RefObject<ExposeParam>}) {
     mdRef.current?.focus()
   }
   return (
-    <TooltipButton icon={previewTypes[type].icon} tooltipText={previewTypes[type].name} onClick={handler}>
-    </TooltipButton>
+    <Tabs defaultValue="code" className="mx-2" onValueChange={handler}>
+      <TabsList className="grid w-full grid-cols-3 gap-0.5">
+        {
+          previewTypes.map((item, index) => (
+            <TabsTrigger className="p-1.5" key={index} value={item.type}>
+              {item.icon}
+            </TabsTrigger>
+          ))
+        }
+      </TabsList>
+    </Tabs>
   )
 }
