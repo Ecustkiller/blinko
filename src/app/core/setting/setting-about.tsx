2cc752225815a002ffd518af97da132dcc152a28
codexu <461229187@qq.com>
2025-02-12 10:41:32 +0800

    fix(#75): 移除检查更新按钮。

--- src/app/core/setting/setting-about.tsx
+++ src/app/core/setting/setting-about.tsx
@@ -1,7 +1,7 @@
 import useSettingStore from "@/stores/setting";
 import { OpenBroswer } from "@/components/open-broswer";
 import { SettingRow, SettingType } from "./setting-base";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 export function SettingAbout({id, icon}: {id: string, icon?: React.ReactNode}) {
   const { version } = useSettingStore()
 
@@ -11,7 +11,7 @@ export function SettingAbout({id, icon}: {id: string, icon?: React.ReactNode}) {
         <span>
           NoteGen v{version}，<OpenBroswer title="查询历史版本" url="https://github.com/codexu/note-gen/releases" />。
         </span>
-        <Button disabled>检查更新</Button>
+        {/* <Button disabled>检查更新</Button> */}
       </SettingRow>
     </SettingType>
   )
