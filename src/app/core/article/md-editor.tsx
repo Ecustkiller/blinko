7ed47415bb7b20651edc4bb5e3507db80ea0ce99
codexu <461229187@qq.com>
2025-02-25 09:47:39 +0800

    feat: 在编辑器中使用上传图片功能

--- src/app/core/article/md-editor.tsx
+++ src/app/core/article/md-editor.tsx
@@ -7,10 +7,16 @@ import CustomToolbar from './custom-toolbar'
 import './style.scss'
 import config from './custom-toolbar/config'
 import { useTheme } from 'next-themes'
+import { toast } from '@/hooks/use-toast'
+import { fileToBase64, uploadFile } from '@/lib/github'
+import { RepoNames } from '@/lib/github.types'
+import useSettingStore from '@/stores/setting'
+import { Store } from '@tauri-apps/plugin-store'
 
 export function MdEditor() {
   const [editor, setEditor] = useState<Vditor>();
   const { currentArticle, saveCurrentArticle, loading } = useArticleStore()
+  const { jsdelivr, accessToken } = useSettingStore()
   const { theme } = useTheme()
   
   function init() {
@@ -25,8 +31,62 @@ export function MdEditor() {
       },
       input: (value) => {
         saveCurrentArticle(value)
+      },
+      upload: {
+        async handler(files: File[]) {
+          const filesUrls = await uploadImages(files)
+          if (vditor) {
+            for (let i = 0; i < filesUrls.length; i++) {
+              vditor.insertValue(`![${files[i].name}](${filesUrls[i]})`)
+            }
+          }
+          return filesUrls.join('\n')
+        }
       }
     })
+  }
+
+  async function uploadImages(files: File[]) {
+    if (!accessToken) {
+      toast({
+        variant: 'destructive',
+        title: '上传失败',
+        description: '上传图片需配置 accessToken',
+      })
+      return ['']
+    }
+    const list = await Promise.all(
+      files.map((file) => {
+        return new Promise<string>(async(resolve, reject) => {
+          if (!file.type.includes('image')) return
+          const t = toast({
+            title: '正在上传图片',
+            description: file.name,
+            duration: 600000,
+          })
+          const fileBase64 = await fileToBase64(file)
+          await uploadFile({
+            ext: file.name.split('.')[1],
+            file: fileBase64,
+            repo: RepoNames.image
+          }).then(async res => {
+            let url = res?.data.content.download_url
+            if (jsdelivr) {
+              const store = await Store.load('store.json');
+              const githubUsername = await store.get('githubUsername')
+              await fetch(`https://purge.jsdelivr.net/gh/${githubUsername}/${RepoNames.image}@main/${res?.data.content.name}`)
+              url = `https://cdn.jsdelivr.net/gh/${githubUsername}/${RepoNames.image}@main/${res?.data.content.name}`
+            }
+            resolve(url)
+          }).catch(err => {
+            reject(err)
+          }).finally(() => {
+            t.dismiss()
+          })
+        });
+      })
+    );
+    return list
   }
 
   function setContent(content: string) {
