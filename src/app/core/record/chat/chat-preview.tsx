a0b1a78e33bc46e4e820292f39bf16b5baaf7590
codexu <461229187@qq.com>
2025-03-05 14:59:21 +0800

    fix: AI 对话框中的链接，采用外部浏览器打开

--- src/app/core/record/chat/chat-preview.tsx
+++ src/app/core/record/chat/chat-preview.tsx
@@ -1,13 +1,34 @@
-import { MdPreview, Themes } from 'md-editor-rt';
+import { ExposeParam, MdPreview, Themes } from 'md-editor-rt';
 import useSettingStore from "@/stores/setting";
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTheme } from 'next-themes'
+import useChatStore from '@/stores/chat';
 
 export default function ChatPreview({text}: {text: string}) {
   const [id] = useState('preview-only');
+  const ref = useRef<ExposeParam>(null);
   const { theme } = useTheme()
   const [mdTheme, setMdTheme] = useState<Themes>('light')
   const { codeTheme, previewTheme } = useSettingStore()
+  const { chats } = useChatStore()
+
+  function bindPreviewLink() {
+    setTimeout(() => {
+      const previewDoms = document.querySelectorAll('.md-editor')
+      for (let index = 0; index < previewDoms.length; index++) {
+        const previewDom = previewDoms[index];
+        if (!previewDom) continue
+        previewDom.querySelectorAll('a').forEach(item => {
+          item.setAttribute('target', '_blank')
+          item.setAttribute('rel', 'noopener noreferrer')
+        })        
+      }
+    }, 100);
+  }
+
+  useEffect(() => {
+    bindPreviewLink()
+  }, [chats])
 
   useEffect(() => {
     setMdTheme(theme as Themes)
@@ -16,6 +37,7 @@ export default function ChatPreview({text}: {text: string}) {
   return <div>
     <MdPreview
       id={id}
+      ref={ref}
       className="flex-1"
       value={text}
       theme={mdTheme}
