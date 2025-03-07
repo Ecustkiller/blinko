64666867434e3e33e628ccc507b4ac9b3e9a8887
codexu <461229187@qq.com>
2025-03-07 11:24:02 +0800

    perf: 👌优化续写逻辑

--- src/app/core/article/custom-toolbar/continue.tsx
+++ src/app/core/article/custom-toolbar/continue.tsx
@@ -11,23 +11,13 @@ export default function Continue({editor}: {editor?: Vditor}) {
     const content = editor?.getValue()
     editor?.focus()
     if (!content) return
-    const currentLineContent = editor?.vditor.ir?.range?.startContainer.nodeValue || ''
-    const currentCursorIndex = content?.indexOf(currentLineContent) + currentLineContent.length
-    const startContent = content.slice(0, currentCursorIndex);
-    const endContent = content.slice(currentCursorIndex, content.length);
+    const selection = document.getSelection();
+    const anchorOffset = selection?.anchorOffset
+    const startContent = content.slice(0, anchorOffset);
+    const endContent = content.slice(anchorOffset, content.length);
     const req = `
-      参考前文：
-      --- 前文开始 ---
-      ${startContent}，
-      --- 前文结束 ---
-
-      参考后文：
-      -- 后文开始 --
-      ${endContent}
-      -- 后文结束 --
-
-      要求在前文和后文中间续写一些内容，直接返回结果，内容不要超过100字。
-      不要与前文或后文内容出现重复的内容。
+      根据前文：“${startContent}” 内容，直接返回续写内容，不要超过50字。
+      内容可以参考后文：“${endContent}”，不要与后文内容重复。
     `
     const res = await fetchAi(req)
     editor?.insertValue(res)
