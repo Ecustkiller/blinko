d58156055a7816352ab6ddb9c83267a51dd784d9
codexu <461229187@qq.com>
2025-03-17 16:22:26 +0800

    feat: 写作编辑器增加选中文本翻译

--- src/app/core/article/custom-toolbar/translation.tsx
+++ src/app/core/article/custom-toolbar/translation.tsx
@@ -1,5 +1,7 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { fetchAi } from "@/lib/ai";
+import { useEffect, useState } from "react";
+import emitter from "@/lib/emitter";
 import { Languages } from "lucide-react";
 import { locales } from "@/lib/locales";
 import useArticleStore from "@/stores/article";
@@ -9,21 +11,45 @@ import useSettingStore from "@/stores/setting";
 import Vditor from "vditor";
 
 export default function Translation({editor}: {editor?: Vditor}) {
+  const [open, setOpen] = useState(false)
   const { loading, setLoading } = useArticleStore()
   const { apiKey } = useSettingStore()
+  const [selectedText, setSelectedText] = useState('')
+  const [range, setRange] = useState<Range | null>(null)
+  const [offsetTop, setOffsetTop] = useState(0)
+  const [offsetLeft, setOffsetLeft] = useState(0)
 
   function openHander(isOpen: boolean) {
-    if (!isOpen) return
+    setOffsetTop(editor?.vditor.toolbar?.elements?.translation?.offsetTop || 0)
+    setOffsetLeft(editor?.vditor.toolbar?.elements?.translation?.offsetLeft || 0)
+    setOpen(isOpen)
+    const preDom = document.querySelector('.vditor-reset') 
+    const _range = document?.createRange()
+    if (preDom && _range) {
+      _range.selectNodeContents(preDom)
+      setRange(_range)
+    }
+    if (isOpen) {
+      setSelectedText(editor?.getSelection() || '')
+    }
   }
   async function handleBlock(locale: string) {
-    editor?.focus()
-    const selectedText = editor?.getSelection()
+    setOpen(false)
     if (selectedText) {
       setLoading(true)
+      editor?.blur()
       const req = `将这段文字：${selectedText}，翻译为${locale}语言，直接返回翻译后的结果。`
       const res = await fetchAi(req)
-      editor?.updateValue(res)
       setLoading(false)
+      if (range) {
+        const selection = document.getSelection()
+        if (selection) {
+          selection.removeAllRanges()
+          selection.addRange(range)
+        }
+      }
+      editor?.focus()
+      editor?.insertValue(res)
     } else {
       toast({
         title: '请先选择一段内容',
@@ -31,20 +57,35 @@ export default function Translation({editor}: {editor?: Vditor}) {
       })
     }
   }
+
+  useEffect(() => {
+    emitter.on('toolbar-translation', () => {
+      openHander(true)
+    })
+    return () => {
+      emitter.off('toolbar-translation', () => {
+        openHander(false)
+      })
+    }
+  }, [])
+
   return (
-    <DropdownMenu onOpenChange={openHander}>
+    <DropdownMenu open={open} onOpenChange={openHander}>
       <DropdownMenuTrigger asChild className="outline-none" disabled={loading || !apiKey}>
         <div>
           <TooltipButton tooltipText="翻译" icon={<Languages />} disabled={loading || !apiKey} />
         </div>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
+      <DropdownMenuContent className="w-56 fixed" style={{ top: offsetTop + 32, left: offsetLeft }}>
         <DropdownMenuLabel>将选中的文本进行翻译</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           {
             locales.map(item => (
-              <DropdownMenuItem key={item} onClick={() => handleBlock(item)}>
+              <DropdownMenuItem key={item} onClick={(e) => {
+                e.preventDefault()
+                handleBlock(item)
+              }}>
                 {item}
               </DropdownMenuItem>
             ))
--- src/lib/locales.ts
+++ src/lib/locales.ts
@@ -2,7 +2,6 @@ export const locales = [
   '简体中文',
   'English',
   '日本語',
-  'Українська', 
   'Français', 
   '한국어', 
   'Português', 
