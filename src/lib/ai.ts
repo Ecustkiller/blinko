8ee85f590631d6b581c04483c137db12a4765f81
codexu <461229187@qq.com>
2025-05-16 16:38:51 +0800

    feat: openai 增加 defaultHeaders

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -38,6 +38,16 @@ async function createOpenAIClient() {
     apiKey: apiKey || '',
     baseURL: baseURL,
     dangerouslyAllowBrowser: true,
+    defaultHeaders:{
+      "x-stainless-arch": null,
+      "x-stainless-lang": null,
+      "x-stainless-os": null,
+      "x-stainless-package-version": null,
+      "x-stainless-retry-count": null,
+      "x-stainless-runtime": null,
+      "x-stainless-runtime-version": null,
+      "x-stainless-timeout": null,
+    },
     ...(proxyUrl ? { httpAgent: proxyUrl } : {})
   })
 }
