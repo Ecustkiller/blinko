7e4050f9dc312c6558ec9abc6d15058ff72512ac
codexu <461229187@qq.com>
2025-05-22 14:26:56 +0800

    feat: 编辑器状态栏增加主要同步方式展示

--- src/app/core/article/custom-footer/index.tsx
+++ src/app/core/article/custom-footer/index.tsx
@@ -2,6 +2,7 @@ import Vditor from 'vditor'
 import Sync from "./sync";
 import History from "./history";
 import TextNumber from "./text-number";
+import PrimarySync from "./primary-sync";
 
 export default function CustomFooter({editor}: {editor?: Vditor}) {
   return <div className="h-6 w-full px-2 border-t shadow-sm items-center flex justify-between overflow-hidden">
@@ -9,6 +10,7 @@ export default function CustomFooter({editor}: {editor?: Vditor}) {
       <TextNumber />
     </div>
     <div className="flex items-center gap-1">
+      <PrimarySync />
       <History editor={editor} />
       <Sync editor={editor} />
     </div>
