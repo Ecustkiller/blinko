6482c77c85ed70bab21e8792baca2896b71b1a21
codexu <461229187@qq.com>
2025-06-05 11:29:37 +0800

    feat: 关于新版本更新说明展示

--- src/app/core/setting/about/updater.tsx
+++ src/app/core/setting/about/updater.tsx
@@ -7,7 +7,9 @@ import useSettingStore from '@/stores/setting';
 import Image from 'next/image';
 import { toast } from '@/hooks/use-toast';
 import { Badge } from '@/components/ui/badge';
-import { ArrowBigRightDash, Loader2 } from 'lucide-react';
+import { ArrowBigRightDash, Link, Loader2 } from 'lucide-react';
+import { getRelease } from '@/lib/github';
+import { open } from '@tauri-apps/plugin-shell';
 
 export default function Updater() {
     const t = useTranslations('settings.about');
@@ -15,11 +17,17 @@ export default function Updater() {
     const [loading, setLoading] = useState(false);
     const { version } = useSettingStore();
     const [update, setUpdate] = useState<Update | null>(null);
+    const [latestBody, setLatestBody] = useState(null);
 
     async function checkUpdate() {
       setChecking(true);
       try {
         setUpdate(await check());
+        getRelease().then((release) => {
+          if (release) {
+            setLatestBody(release.body)
+          }
+        })
       } catch (error) {
         toast({
           title: t('checkError'),
@@ -54,38 +62,57 @@ export default function Updater() {
       }
     }
 
+    function openRelease() {
+      open('https://github.com/NoteGen/NoteGen/releases');
+    }
+
     useEffect(() => {
       checkUpdate();
     }, []);
 
     return (
-      <div className="flex justify-between w-full items-center">
-        <div className="flex items-center gap-4">
-          <div>
-            <Image src="/app-icon.png" alt="logo" className="size-24 dark:invert" width={0} height={0} />
-          </div>
-          <div className="h-24 flex flex-col justify-between">
-            <span className="text-2xl font-bold flex items-center gap-2">NoteGen</span>
-            <span>
-              {t('desc')}
-            </span>
-            <div className="flex items-center gap-2">
-              <Badge variant="outline">v{version}</Badge>
-              {
-                update ? (
-                  <>
-                    <ArrowBigRightDash className="size-4" />
-                    <Badge className="bg-green-500 text-white" variant="outline">v{update.version}</Badge>
-                  </>
-                ) : null
-              }
+      <div className="flex flex-col gap-4 w-full">
+        <div className="flex justify-between w-full items-center">
+          <div className="flex items-center gap-4">
+            <div>
+              <Image src="/app-icon.png" alt="logo" className="size-24 dark:invert" width={0} height={0} />
+            </div>
+            <div className="h-24 flex flex-col justify-between">
+              <span className="text-2xl font-bold flex items-center gap-2">NoteGen</span>
+              <span>
+                {t('desc')}
+              </span>
+              <div className="flex items-center gap-2">
+                <Badge variant="outline">v{version}</Badge>
+                {
+                  update ? (
+                    <>
+                      <ArrowBigRightDash className="size-4" />
+                      <Badge className="bg-green-500 text-white" variant="outline">v{update.version}</Badge>
+                    </>
+                  ) : null
+                }
+              </div>
             </div>
           </div>
+          <Button disabled={!update || loading || checking} onClick={checkVersion}>
+            {checking || loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+            {checking ? t('checkUpdate') : update ? t('updateAvailable') : t('noUpdate')}
+          </Button>
         </div>
-        <Button disabled={!update || loading || checking} onClick={checkVersion}>
-          {loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
-          {checking ? t('checkUpdate') : update ? t('updateAvailable') : t('noUpdate')}
-        </Button>
+        {
+          update && latestBody ? (
+            <div className="mt-8 p-4 border rounded-md">
+              <div className="flex items-center gap-2 justify-between mb-4">
+                <h1 className="text-lg font-bold">NoteGen v{update.version}</h1>
+                <Button size="sm" variant="outline" onClick={openRelease}>
+                  <Link className='!size-3' />Release
+                </Button>
+              </div>
+              <p className="text-sm text-muted-foreground whitespace-pre-line">{latestBody}</p>
+            </div>
+          ) : null
+        }
       </div>
-    )
+    ) 
 }
