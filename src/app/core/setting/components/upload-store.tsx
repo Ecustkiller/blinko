0bc88269be72ba4ee67dc2bc8faefafdaa77ffdb
codexu <461229187@qq.com>
2025-07-22 13:37:31 +0800

    feat: Gitlab 上传配置和下载配置

--- src/app/core/setting/components/upload-store.tsx
+++ src/app/core/setting/components/upload-store.tsx
@@ -6,6 +6,7 @@ import { Store } from "@tauri-apps/plugin-store";
 import { RepoNames } from "@/lib/github.types";
 import { uint8ArrayToBase64, uploadFile as uploadGithubFile, getFiles as githubGetFiles, decodeBase64ToString } from "@/lib/github";
 import { getFiles as giteeGetFiles, uploadFile as uploadGiteeFile } from "@/lib/gitee";
+import { uploadFile as uploadGitlabFile, getFiles as gitlabGetFiles, getFileContent as gitlabGetFileContent } from "@/lib/gitlab";
 import { toast } from "@/hooks/use-toast";
 import { useState } from "react";
 import { isMobileDevice } from "@/lib/check";
@@ -19,44 +20,56 @@ export default function UploadStore() {
   const t = useTranslations('settings.uploadStore')
 
   async function upload() {
-    const res = await confirm(t('uploadConfirm'))
-    if (!res) return
+    const confirmRef = await confirm(t('uploadConfirm'))
+    if (!confirmRef) return
     setUploading(true)
     const path = '.settings'
     const filename = 'store.json'
     const file = await readFile('store.json', { baseDir: BaseDirectory.AppData });
     const store = await Store.load('store.json');
     const primaryBackupMethod = await store.get('primaryBackupMethod')
-    if (primaryBackupMethod === 'github') {
-      const files = await githubGetFiles({ path: `${path}/${filename}`, repo: RepoNames.sync })
-      const res = await uploadGithubFile({
-        ext: 'json',
-        file: uint8ArrayToBase64(file),
-        repo: RepoNames.sync,
-        path,
-        filename,
-        sha: files?.sha,
-      })
-      if (res) {
-        toast({
-          description: t('uploadSuccess'),
+    let files;
+    let res;
+    switch (primaryBackupMethod) {
+      case 'github':
+        files = await githubGetFiles({ path: `${path}/${filename}`, repo: RepoNames.sync })
+        res = await uploadGithubFile({
+          ext: 'json',
+          file: uint8ArrayToBase64(file),
+          repo: RepoNames.sync,
+          path,
+          filename,
+          sha: files?.sha,
         })
-      }
-    } else if (primaryBackupMethod === 'gitee') {
-      const files = await giteeGetFiles({ path: `${path}/${filename}`, repo: RepoNames.sync })
-      const res = await uploadGiteeFile({
-        ext: 'json',
-        file: uint8ArrayToBase64(file),
-        repo: RepoNames.sync,
-        path,
-        filename,
-        sha: files?.sha,
-      })
-      if (res) {
-        toast({
-          description: t('uploadSuccess'),
+        break;
+      case 'gitee':
+        files = await giteeGetFiles({ path: `${path}/${filename}`, repo: RepoNames.sync })
+        res = await uploadGiteeFile({
+          ext: 'json',
+          file: uint8ArrayToBase64(file),
+          repo: RepoNames.sync,
+          path,
+          filename,
+          sha: files?.sha,
         })
-      }
+        break;
+      case 'gitlab':
+        files = await gitlabGetFiles({ path: `${path}/${filename}`, repo: RepoNames.sync })
+        const storeFile = files?.find(file => file.name === filename)
+        res = await uploadGitlabFile({
+          ext: 'json',
+          file: uint8ArrayToBase64(file),
+          repo: RepoNames.sync,
+          path,
+          filename,
+          sha: storeFile?.sha || '',
+        })
+        break;
+    }
+    if (res) {
+      toast({
+        description: t('uploadSuccess'),
+      })
     }
     setUploading(false)
   }
@@ -70,10 +83,16 @@ export default function UploadStore() {
     const store = await Store.load('store.json');
     const primaryBackupMethod = await store.get('primaryBackupMethod')
     let file;
-    if (primaryBackupMethod === 'github') {
-      file = await githubGetFiles({ path: `${path}/${filename}`, repo: RepoNames.sync })
-    } else if (primaryBackupMethod === 'gitee') {
-      file = await giteeGetFiles({ path: `${path}/${filename}`, repo: RepoNames.sync })
+    switch (primaryBackupMethod) {
+      case 'github':
+        file = await githubGetFiles({ path: `${path}/${filename}`, repo: RepoNames.sync })
+        break;
+      case 'gitee':
+        file = await giteeGetFiles({ path: `${path}/${filename}`, repo: RepoNames.sync })
+        break;
+      case 'gitlab':
+        file = await gitlabGetFileContent({ path: `${path}/${filename}`, ref: 'main', repo: RepoNames.sync })
+        break;
     }
     if (file) {
       const configJson = decodeBase64ToString(file.content)
